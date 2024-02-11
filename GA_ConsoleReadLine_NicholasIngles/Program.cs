6f16f0723ab3026805d95575f3e670bc13205aff
Nicholas Ingles <nringles@student.rtc.edu>
2024-02-10 18:12:17 -0800

    Completed Completely

--- GA_ConsoleReadLine_NicholasIngles/Program.cs
+++ GA_ConsoleReadLine_NicholasIngles/Program.cs
@@ -8,6 +8,13 @@ class Program
         Console.WriteLine("What is your name?");
 
         string userName = Console.ReadLine();
-    }
+        Console.WriteLine("\nHello, " + userName + "!");
+
+        Console.WriteLine("\nWhat is your favorite color?");
+
+        string favoriteColor = Console.ReadLine();
+        Console.WriteLine("\nAh, " + favoriteColor + " is a great color!");
+        Console.WriteLine("\n" + userName + ", who likes " + favoriteColor + ", welcome to the program!");
+    }       
 
 }
