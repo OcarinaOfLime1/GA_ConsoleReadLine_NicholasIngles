9312a083ee44358250f54be881d299949e34413e
Nicholas Ingles <nringles@student.rtc.edu>
2024-02-10 17:57:16 -0800

    Add project files.

---
+++ GA_ConsoleReadLine_NicholasIngles/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What is your name?");
+
+        string userName = Console.ReadLine();
+    }
+
+}
